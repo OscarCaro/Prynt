06cbeb4942d185a2088a0c3abec559a20df36c9d
Óscar Caro <oscaro@ucm.es>
2020-11-27 10:51:45 +0100

    Corrección bug en función stateToSvg

--- js/pmgr.js
+++ js/pmgr.js
@@ -23,23 +23,25 @@ import * as Pmgr from './pmgrapi.js'
 // en respuesta a algún evento.
 //
 
+
 function statusToSVG(state, desiredSize) {
+  const PS = Pmgr.PrinterStates;
   switch (state) {
-    case PrinterStates.PRINTING:
+    case PS.PRINTING:
       return `
-      <svg width="1em" height="1em" viewBox="0 0 16 16"
-        class="bi bi-printer-fill" fill="dark"
-        xmlns="http://www.w3.org/2000/svg">
+      <svg width="${desiredSize}em" height="${desiredSize}em" viewBox="0 0 16 16" class="bi bi-printer-fill"
+        fill="currentColor" xmlns="http://www.w3.org/2000/svg">
         <path d="M5 1a2 2 0 0 0-2 2v1h10V3a2 2 0 0 0-2-2H5z" />
         <path fill-rule="evenodd"
-          d="M11 9H5a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1z" />
+            d="M11 9H5a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1z" />
         <path fill-rule="evenodd"
-          d="M0 7a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-1v-2a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v2H2a2 2 0 0 1-2-2V7zm2.5 1a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z" /> 
-          `;
+            d="M0 7a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-1v-2a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v2H2a2 2 0 0 1-2-2V7zm2.5 1a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z" />
+      </svg> <!-- icono impresora -->
+      `;
       break;
-    case PrinterStates.PAUSED:
+    case PS.PAUSED:
       return `
-      <svg width="1em" height="1em" viewBox="0 0 16 16"
+      <svg width="${desiredSize}em" height="${desiredSize}em" viewBox="0 0 16 16"
         class="bi bi-printer-fill" fill="dark"
         xmlns="http://www.w3.org/2000/svg">
         <path d="M5 1a2 2 0 0 0-2 2v1h10V3a2 2 0 0 0-2-2H5z" />
@@ -47,25 +49,13 @@ function statusToSVG(state, desiredSize) {
           d="M11 9H5a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1z" />
         <path fill-rule="evenodd"
           d="M0 7a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-1v-2a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v2H2a2 2 0 0 1-2-2V7zm2.5 1a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z" /> 
-          `;
-
-      break;
-    case PrinterStates.NO_INK:
-      return `
-      <svg width="1em" height="1em" viewBox="0 0 16 16"
-        class="bi bi-printer-fill" fill="dark"
-        xmlns="http://www.w3.org/2000/svg">
-        <path d="M5 1a2 2 0 0 0-2 2v1h10V3a2 2 0 0 0-2-2H5z" />
-        <path fill-rule="evenodd"
-          d="M11 9H5a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1z" />
-        <path fill-rule="evenodd"
-          d="M0 7a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-1v-2a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v2H2a2 2 0 0 1-2-2V7zm2.5 1a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z" /> 
+      </svg> <!-- icono impresora -->
           `;
 
       break;
-    case PrinterStates.NO_PAPER:
+    case PS.NO_INK:
       return `
-      <svg width="1em" height="1em" viewBox="0 0 16 16"
+      <svg width="${desiredSize}em" height="${desiredSize}em" viewBox="0 0 16 16"
         class="bi bi-printer-fill" fill="dark"
         xmlns="http://www.w3.org/2000/svg">
         <path d="M5 1a2 2 0 0 0-2 2v1h10V3a2 2 0 0 0-2-2H5z" />
@@ -73,21 +63,25 @@ function statusToSVG(state, desiredSize) {
           d="M11 9H5a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1z" />
         <path fill-rule="evenodd"
           d="M0 7a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-1v-2a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v2H2a2 2 0 0 1-2-2V7zm2.5 1a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z" /> 
+      </svg> <!-- icono impresora -->
+          `;
+
+      break;
+    case PS.NO_PAPER:
+      return `
+      <svg width="${desiredSize}em" height="${desiredSize}em" viewBox="0 0 16 16"
+        class="bi bi-printer-fill" fill="dark"
+        xmlns="http://www.w3.org/2000/svg">
+        <path d="M5 1a2 2 0 0 0-2 2v1h10V3a2 2 0 0 0-2-2H5z" />
+        <path fill-rule="evenodd"
+          d="M11 9H5a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1z" />
+        <path fill-rule="evenodd"
+          d="M0 7a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-1v-2a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v2H2a2 2 0 0 1-2-2V7zm2.5 1a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z" /> 
+      </svg> <!-- icono impresora -->    
           `;
 
       break;
     default:
-      return `
-      <svg width="1em" height="1em" viewBox="0 0 16 16"
-        class="bi bi-printer-fill" fill="dark"
-        xmlns="http://www.w3.org/2000/svg">
-        <path d="M5 1a2 2 0 0 0-2 2v1h10V3a2 2 0 0 0-2-2H5z" />
-        <path fill-rule="evenodd"
-          d="M11 9H5a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1z" />
-        <path fill-rule="evenodd"
-          d="M0 7a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-1v-2a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v2H2a2 2 0 0 1-2-2V7zm2.5 1a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z" /> 
-          `;
-      break;
   }
 }
 
@@ -128,8 +122,10 @@ function createPrinterItem(printer) {
                 <span class="badge badge-pill ${pillClass[printer.status]}">${printer.status}</span>
             -->
 
-            <div class="col">
-              ${statusToSVG(printer.status, 10)} 
+            <div class="col">  
+              <div class="text right">            
+                  ${statusToSVG(printer.status, 3)}
+              </div>
             </div>
           </div>
         </button>
